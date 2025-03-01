4a522b73592703c6c0028db0b17c5d6d86f13f13
Mariia <mmnnosova@gmail.com>
2025-03-02 00:53:33 +0400

    реализован класс Coffee

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,35 +1,41 @@
-/**
- * Класс Coffee
- *
- * Поля (примерная идея):
- *   - private String name;       // Название кофе
- *   - private double basePrice;  // Базовая цена
- *
- * Геттеры/сеттеры:
- *   - getName(), setName(...)
- *   - getBasePrice(), setBasePrice(...)
- *
- * Методы (примерные идеи):
- *   - calculatePriceWithSugar(Sugar sugar): double
- *       (может возвращать basePrice + некая надбавка за сахар, или просто basePrice, если надбавки нет)
- *
- *   - toString() (не обязательно, но может упростить вывод информации)
- */
 public class Coffee {
-    // Напишите код здесь (объявление полей)
+    private String name;
+    private double basePrice;
+    private double finalPrice;
 
-    // Напишите код здесь (конструктор/конструкторы)
+    public Coffee(String name, double basePrice) {
+        this.name = name;
+        this.basePrice = basePrice;
+        this.finalPrice = basePrice;
+    }
 
-    // Напишите код здесь (геттеры/сеттеры)
+    public String getName() {
+        return name;
+    }
 
-    // public double calculatePriceWithSugar(Sugar sugar) {
-    //     // Напишите код здесь:
-    //     // Пример:
-    //     // double finalPrice = basePrice + sugar.getSugarPrice();
-    //     // return finalPrice;
-    // }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    // public String toString() {
-    //     // Напишите код здесь (опционально)
-    // }
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+        this.finalPrice = basePrice;
+    }
+
+    public double getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void calculatePriceWithSugar(Sugar sugar) {
+        this.finalPrice = basePrice + sugar.getSugarPrice();
+    }
+
+    public String toString() {
+        return "Кофе: " + name + " (Цена: " + finalPrice + " руб.)";
+    }
 }
+
